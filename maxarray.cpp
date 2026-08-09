ad62ecb7f8bfe94beeb4ecc09770710886ed0368
Harsh <hb438384@gmail,com>
2026-08-09 20:48:17 +0530

    max for array

---
+++ maxarray.cpp
@@ -0,0 +1,15 @@
+// Homework : Calculate the product of all the elements in the given array.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {5,4,3,45,9,65,89};
+    int max = 1;
+    
+    for (int i = 0; i < 7; i++)
+    {
+        max = arr[i];
+    }
+        
+    cout <<"max = "<< max;
+}
