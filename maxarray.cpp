7efae353c551d5d4e74062d6691eaea27cffecd0
Harsh <hb438384@gmail,com>
2026-08-09 20:50:04 +0530

    max

--- maxarray.cpp
+++ maxarray.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
-    int arr[] = {5,4,3,45,9,65,89};
+    int arr[] = {5, 4, 3, 45, 9, 65, 89};
     int max = 1;
-    
+
     for (int i = 0; i < 7; i++)
     {
         max = arr[i];
     }
-        
-    cout <<"max = "<< max;
+
+    cout << "max = " << max;
 }
